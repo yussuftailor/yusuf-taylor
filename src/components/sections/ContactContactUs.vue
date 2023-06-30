da0f1432575fcc58fd3fde1abb9c336b98f8acea
Vortex <yussuftailorIT@gmail.com>
2023-06-30 20:19:44 +0700

    update contact number

--- src/components/sections/ContactContactUs.vue
+++ src/components/sections/ContactContactUs.vue
@@ -5,7 +5,7 @@
                 <h2 class="text-left w-full font-philosopher font-bold text-white text-2xl md:text-4xl">Dapatkan Setelan
                     Terbaik</h2>
                 <p class="text-left w-full mt-4 font-philosopher text-base text-white max-w-2xl ">Kami memberikan layanan profesional untuk setiap pemesanan jahitan yang Anda buat. Anda bisa membuat pesanan sesuai dengan ukuran, bahan, dan model yang Anda inginkan.</p>
-                <a href="https://wa.me/6282242626274" target="_blank"
+                <a href="https://wa.me/85158855295" target="_blank"
                     class="font-philosopher w-fit rounded-md bg-brand px-3.5 py-2.5 text-sm font-semibold mt-8"
                     style="color:#FFFFFF;">Hubungi Kami
                 </a>
